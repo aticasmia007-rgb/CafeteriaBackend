9ccecd87ab872c6f9be9e65805ed79ae8e485eb7
JA <42705473+fnbutton@users.noreply.github.com>
2026-05-12 01:48:29 +0200

    Comment out collectstatic command in entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-uv run python manage.py collectstatic --noinput
+# uv run python manage.py collectstatic --noinput
 uv run python manage.py migrate --noinput
 
 exec uv run gunicorn config.wsgi:application \
